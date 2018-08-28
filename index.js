64c6444adbe553f7979fecbefc2fc2a122e91725
unknown <choopk1994@gmail.com>
2018-08-29 02:29:53 +0800

    changed to discord.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var Discord = require('discord.io');
+const Discord = require("discord.js");
+const client = new Discord.Client();
 var logger = require('winston');
-var auth = require('./auth.json');
-
-
+//var auth = require('./auth.json');
+const fs = require("fs")
+const config = require("./config.json");
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -14,42 +15,27 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-			break;
-			
-		    case 'intro':
-				bot.sendMessage({
-                    to: channelID,
-                    message: 'Welcome mutherfuckerss!'
-                });
-				
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+client.login(config.token);
+
+const prefix = "!";
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (message.content.startsWith(prefix + "Bing")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+
+  if(message.content.startsWith(config.prefix + "prefix")) {
+    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+    let newPrefix = message.content.split(" ").slice(1, 2)[0];
+    console.log(newPrefix);
+    // change the configuration in memory
+    config.prefix = newPrefix;
+    // Now we have to save the file.
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+  }
 });
 
 
