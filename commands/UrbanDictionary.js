e6fdb9ef9224791e40d2f22bd53df13afed55fd9
unknown <choopk1994@gmail.com>
2018-09-07 00:30:25 +0800

    urban

---
+++ commands/UrbanDictionary.js
@@ -0,0 +1,15 @@
+
+const Discord = require("discord.js");
+const fetch = require("snekfetch");
+
+module.exports.run = async (bot, message, args) => {
+    
+    if(!args){
+        return msg.channel.reply("Give me something to search");
+    }
+}
+
+ 
+module.exports.help = {
+  name: "Urban"
+}
